3302dedaa41b266184daba48627cb9f622fc38b2
Sahir Butt <sahir.butt@outlook.com>
2024-03-16 15:27:46 +0000

    added defensive programming

--- age-quiz.py
+++ age-quiz.py
@@ -1,29 +1,33 @@
 # This program takes the user's age as input and provides different messages
 # based on certain age ranges:
 
+try:
 # Prompt the user to input their age and convert it to an integer
-age = int(input("Enter your age: "))
+    age = int(input("Enter your age: "))
 
 # Check if the age is less than 13, print message
-if age < 13:
-    print("You qualify for the kiddie discount.")
+    if age < 13:
+        print("You qualify for the kiddie discount.")
 
 # Check if the age is exactly 21, print message
-elif age == 21:
-    print ("Congrats on your 21st!"), 
+    elif age == 21:
+        print ("Congrats on your 21st!"), 
 
 # Check if the age is between 40 and 65 (exclusive),print message
-elif 40 <= age < 65:
-    print("You're over the hill")
+    elif 40 <= age < 65:
+        print("You're over the hill")
 
 # Check if the age is between 65 and 100 (inclusive), print message
-elif 65 <= age <= 100:
-    print ("Enjoy your retirement!")
+    elif 65 <= age <= 100:
+        print ("Enjoy your retirement!")
 
 # Check if the age is greater than 100, print message
-elif age > 100:
-     print ("Sorry, you're dead!")
+    elif age > 100:
+        print ("Sorry, you're dead!")
 
 # If none of the above conditions are met, print a message
-else:
-    print("Age is but a number")
+    else:
+        print("Age is but a number")
+
+except ValueError:
+    print("Please enter a valid age as a number.")
